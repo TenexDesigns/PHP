8d23e152ac59b2dbf6637cfde9389f90cb8420f7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-04 18:28:12 +0300

    Update Php_1.kt

--- Php_1.kt
+++ Php_1.kt
@@ -1,1 +1,35 @@
 This is the starting part of the php course
+
+
+<?php
+
+echo "I love and adore pizzza <br>";
+echo "I love and adore George Gacau <br>";
+echo "It's reallly goood"
+// To generate line breakes or to go to the next line use <br>
+  // This is a coommment
+
+  /*
+  This is a 
+  multiline  comment
+  
+  PHP FILES CAN CONTAIN HTML ,CSS JAVASCRIPT AND PHP
+  */
+
+?>
+  
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+
+<button> Order bbbpizza</button>
+    
+</body>
+</html>
