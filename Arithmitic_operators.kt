2a44f3b1afb6c3292ea6fe5dc9bdcdbada0a6a3d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-05 08:06:31 +0300

    Create Arithmitic_operators.kt

---
+++ Arithmitic_operators.kt
@@ -0,0 +1,272 @@
+ARITHMETIC OPERATORS
+
++ 
+-
+*
+/
+**
+%
+  
+  INCREMEMNT AND DECREMENT OPERATORS
+
+++
+--
+
+OPERATOR PRECEDENCE
+()
+**
+* / %
++ -  
+
+
+
+              ARITHMETIC OPERATORS
+
+ADDITION
+
+$X = 10;
+$Y =2;
+$Z= null;
+
+$z = $x + $y;
+
+echo $z       ---------------> returns 12 on browser
+
+
+
+            
+
+SUBTRACTION
+
+$X = 10;
+$Y =2;
+$Z= null;
+
+$z = $x - $y;
+
+echo $z       ---------------> returns 8 on browser
+
+
+
+MULTIPLICATION
+
+$X = 10;
+$Y =2;
+$Z= null;
+
+$z = $x * $y;
+
+echo $z       ---------------> returns 20 on browser
+
+
+POWER
+
+$X = 10;
+$Y =2;
+$Z= null;
+
+$z = $x ** $y;
+
+echo $z       ---------------> returns 100 on browser
+
+
+
+MODULAS
+
+$X = 10;
+$Y =3;
+$Z= null;
+
+$z = $x + $y;
+
+echo $z       ---------------> returns 1 on browser.It returns the remianer of a divsion. This can be used to determine if a number is even or not
+
+
+
+              
+
+DIVISION
+
+$X = 10;
+$Y =2;
+$Z= null;
+
+$z = $x / $y;
+
+echo $z       ---------------> returns 5 on browser
+
+
+
+
+
+
+                                             INCREMENT AND DECREMENT OPERATORS
+
+
+INCREMENT OPERATORS ++
+
+  
+  
+  WE CAN USE THE INCREMENT OPERATOR TO INCREASE THE VALUE BY ONE
+
+$counter = 0
+
+$counter ++;
+
+echo $counter;  This increases the value by one. So this will produce 1
+
+
+
+DECREMENT OPERATOR --
+  
+  
+    
+  WE CAN USE THE INCREMENT OPERATOR TO DECREASE THE VALUE BY ONE
+
+$counter = 0
+
+$counter --;
+
+echo $counter;  This decreases the value by one. So this ill produce -1
+  
+  
+
+
+WE CAN ALSO INCREASE THE VALUESBY MORE THAN ONE
+
+
+$counter = 0
+
+$counter += 3;
+
+echo $counter;  This increases the value by three. So this ill produce 3
+
+
+WE CAN ALSO DECREASE VALUES BY MORE THAN ONE
+
+
+$counter = 0
+
+$counter -=4;
+
+echo $counter;  This decreases the value by 4. So this ill produce -4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+iiii
