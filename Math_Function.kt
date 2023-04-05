81a19cd0586c9d7426382b8a133266dfca3390fc
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-05 11:09:53 +0300

    Create Math_Function.kt

---
+++ Math_Function.kt
@@ -0,0 +1,108 @@
+   There some math functions in php
+
+
+abs(-9)   - return 9 . It gives how far from zero a nuber is
+
+round(3.56) - ROunds a number to the nearest whole number
+
+
+floor(4.67) - returns 4 . It truncates the decimal part
+
+ceil (4.67)  - returns 5 - It rounds up the number.
+
+pow(3,2) - returns 9 - It powes the numbers
+
+sqrt(100) - returns the square root of 100 which is 10.
+
+max(3,5,6) - finds the max vaue , which in this case is 6.
+
+min(3,4,5) - returns 3
+pi() - gives the pi value
+
+rand(1,6) - This is used to generate a random numbert between 1 and 2 billion. To give constarints ,addd the least and maximamum values i.e r.g  from 1 to 6 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
