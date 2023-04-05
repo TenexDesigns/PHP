f4135e4490ba59639bb2fa265f2d65ff47853ead
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-05 07:31:19 +0300

    Update Variable_dataTypes_Php.kt

--- Variable_dataTypes_Php.kt
+++ Variable_dataTypes_Php.kt
@@ -1,7 +1,26 @@
 
 
 VARIABLE -  a reusable container that holds data,
-string ,integer ,float ,boolean
+EXAMPLES OF DATA -> string ,integer ,float ,boolean
+
+To create a variable
+
+$Variable_name  ="variable _value"
+
+
+To display the variable
+
+echo$vaiable_name 
+
+HERE IS AN EXAMPLE
+
+
+<?php
+
+    $name = "George Gacau"
+    echo $name
+
+?>
 
 
 
@@ -12,13 +31,46 @@ string ,integer ,float ,boolean
 
 
 
+STRING LITERALS
+
+
+
+
+<?php
+
+    $name = "George Gacau"
+
+    echo "Hello {$name}"
+
+?>
 
 
 
 
 
+TO DISPLAY A DOLLAR SIGN IN PHP, YOU HAVE TO USE THE BACK STROKE "\",SO AS NOT TO CONFUSE PHP.
 
 
+$price = 4.99;
+
+echo " The food here costs \${$price}";
+
+
+
+BOOLEAN DATA TYPES
+THey are either true or false.
+In php ,it is not possible to dispaly a boolean on the browser.
+
+
+<?php
+
+    $good = true;
+    $bad = false
+
+    echo "Hello {$good}"  --> This wil display a 1 on the browerser
+    echo "Hello {$bad}"   --> This will display nothing on the browerser.
+
+?>
 
 
 
