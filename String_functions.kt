9c86ddac5f5155834f51ebc41454f0d5ee867e24
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-06 12:56:46 +0300

    Create String_functions.kt

---
+++ String_functions.kt
@@ -0,0 +1,162 @@
+$user_name = "Bro_code";
+$phone = "123-333-44";
+
+
+$username = strtolower($username);  --> "bro_code"
+$username = strtoupper($username)   --> "BRO_CODE"
+$phone = str_replace("-","/".$phone); --> "123/333/44"
+
+
+PHP provides a wide range of built-in functions for working with strings. Here are some of the most commonly used string functions in PHP:
+
+strlen(): Returns the length of a string.
+
+strpos(): Returns the position of the first occurrence of a substring in a string.
+
+substr(): Returns a portion of a string.
+
+strtolower(): Converts a string to lowercase.
+
+strtoupper(): Converts a string to uppercase.
+
+trim(): Removes whitespace or other characters from the beginning and end of a string.
+
+str_replace(): Replaces all occurrences of a substring with another string.
+
+str_split(): Splits a string into an array.
+
+implode(): Joins array elements with a string.
+
+explode(): Splits a string into an array using a specified delimiter.
+
+ucfirst(): Converts the first character of a string to uppercase.
+
+ucwords(): Converts the first character of each word in a string to uppercase.
+
+strcmp(): Compares two strings.
+
+strcasecmp(): Compares two strings without considering case.
+
+sprintf(): Formats a string using placeholders.
+
+
+These functions are just a few examples of what you can do with strings in PHP.
+There are many more functions available in the PHP string library that can help you manipulate and work with strings in a variety of ways.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+..
